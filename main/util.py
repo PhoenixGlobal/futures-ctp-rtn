59957994f261bc1e6702533444e95badcdce4887
wxf <wxf@tensorcorp.com>
2026-09-24 03:05:02 +0000

    fix: field

--- main/util.py
+++ main/util.py
@@ -2,16 +2,17 @@ from typing import Any
 from fommon.api.qry_position import QryPositionItem
 
 def cook_raw_position(raw: dict[str, Any]):
+	data: dict[str, Any] = raw['data']
 	return QryPositionItem(
-		InstrumentID=raw['InstrumentID'],
-		posi_direction=cook_raw_posi_direction(raw['PosiDirection']),
-		position_date=cook_raw_position_date(raw['PositionDate']),
-		YdPosition=raw['YdPosition'],
-		Position=raw['Position'],
-		OpenVolume=raw['OpenVolume'],
-		CloseVolume=raw['CloseVolume'],
-		OpenAmount=raw['OpenAmount'],
-		CloseAmount=raw['CloseAmount'],
+		InstrumentID=data['InstrumentID'],
+		posi_direction=cook_raw_posi_direction(data['PosiDirection']),
+		position_date=cook_raw_position_date(data['PositionDate']),
+		YdPosition=data['YdPosition'],
+		Position=data['Position'],
+		OpenVolume=data['OpenVolume'],
+		CloseVolume=data['CloseVolume'],
+		OpenAmount=data['OpenAmount'],
+		CloseAmount=data['CloseAmount'],
 	)
 
 def cook_raw_posi_direction(raw: str):
